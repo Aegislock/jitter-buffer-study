a41b79316bf78f9dddb8ccf3fb0e917000c0337a
Felix Tong <tongfelix000@gmail.com>
2025-06-25 15:52:08 -0700

    Notes and fixessrc/main/java/me/fetong/jitterbuffer/OpusTestEncoder.java

--- src/main/java/me/fetong/jitterbuffer/OpusTestEncoder.java
+++ src/main/java/me/fetong/jitterbuffer/OpusTestEncoder.java
@@ -10,7 +10,6 @@ import java.nio.ByteOrder;
 import io.github.jaredmdobson.concentus.*;
 
 public class OpusTestEncoder {
-    private final int VOIP = 2048;
     private File file;
     private int frameSize;
     private final OpusEncoder encoder;
@@ -31,8 +30,15 @@ public class OpusTestEncoder {
     private List<JitterPacket> encodeAndPacketize(List<short[]> frames) throws IOException, UnsupportedAudioFileException, OpusException{
         List<JitterPacket> bufferInput = new ArrayList<>();
         for (int i = 0; i < frames.size(); i++) {
+            // 1276 because of Opus frame size limit of 1275 + 1 frame for a table of contents / padding  
             byte[] encodedBuffer = new byte[1276];
+            // 960 samples / frame because Opus has a few valid frame sizes for 48kHz mono channel. 
+            // 400 samples is invalid beacuse it does not fit a nice frame duration and Opus will not
+            // try to encode partial frames. 
+            // TLDR: Opus enforces strict frame lengths
             int len = this.encoder.encode(frames.get(i), 0, 960, encodedBuffer, 0, encodedBuffer.length);
+            // There may be empty space at the end of encodedBuffer that is not filled
+            // We use copyOf here to only copy useful information into encodedFrame
             byte[] encodedFrame = Arrays.copyOf(encodedBuffer, len);
             JitterPacket packet = new JitterPacket(encodedFrame, i * frameSize, frameSize, i, 0, 0);
             bufferInput.add(packet);
@@ -45,6 +51,9 @@ public class OpusTestEncoder {
         AudioInputStream audioStream = AudioSystem.getAudioInputStream(this.file);
         // Validate the correct format (16-bit PCM)
         AudioFormat format = audioStream.getFormat();
+        // Check signed-ness and if the audio sample size is 16-bits
+        // Decoding logic aassumes each sample is exactly 2 bytes and maps to a signed short
+        // If format were different, the conversion would interpet the bytes incorrectly
         if (format.getEncoding() != AudioFormat.Encoding.PCM_SIGNED || format.getSampleSizeInBits() != 16) {
             throw new UnsupportedAudioFileException("Only 16-bit signed PCM supported");
         }
@@ -57,7 +66,8 @@ public class OpusTestEncoder {
             baos.write(buffer, 0, bytesRead); 
         }
         byte[] pcmBytes = baos.toByteArray();
-        ByteBuffer bb = ByteBuffer.wrap(pcmBytes).order(ByteOrder.LITTLE_ENDIAN);
+        // Wraps byte[] array so we can read any type of data from it (short, int, long, etc.)
+        ByteBuffer bb = ByteBuffer.wrap(pcmBytes).order(ByteOrder.LITTLE_ENDIAN); // little endian beacuse WAV is decoded from LSB to MSB
         short[] pcmShorts = new short[pcmBytes.length / 2];
 
         for (int i = 0; i < pcmShorts.length; i++) {
